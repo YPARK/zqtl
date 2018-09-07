ec9337d2e8c325a47807fda2aa7b2bb303edf5df
Yongjin Park <ypp@csail.mit.edu>
2018-09-07 17:24:32 -0400

    in noDE smoothness

--- src/rcpp_zqtl_mediation.hh
+++ src/rcpp_zqtl_mediation.hh
@@ -189,7 +189,12 @@ Rcpp::List impl_fit_med_zqtl(const effect_y_mat_t& yy,        // z_y
     }
     param_unmed = param_rcpp_list(theta_unmed);
   } else {
-    auto theta_unmed = make_dense_spike_slab<Scalar>(Vt.cols(), Y.cols(), opt);
+
+    /////////////////////////
+    // this must be smooth //
+    /////////////////////////
+
+    auto theta_unmed = make_dense_slab<Scalar>(Vt.cols(), Y.cols(), opt);
     auto eta_unmed = make_regression_eta(Vt, Y, theta_unmed);
     eta_unmed.init_by_dot(Y, opt.jitter());
     llik = impl_fit_eta_delta(
@@ -343,7 +348,7 @@ Rcpp::List impl_fit_fac_med_zqtl(const effect_y_mat_t& yy,        // z_y
 
     out_unmed_param = param_rcpp_list(theta_unmed);
   } else {
-    auto theta_unmed = make_dense_spike_slab<Scalar>(Vt.cols(), Y.cols(), opt);
+    auto theta_unmed = make_dense_slab<Scalar>(Vt.cols(), Y.cols(), opt);
     auto eta_unmed = make_regression_eta(Vt, Y, theta_unmed);
     eta_unmed.init_by_dot(Y, opt.jitter());
 
